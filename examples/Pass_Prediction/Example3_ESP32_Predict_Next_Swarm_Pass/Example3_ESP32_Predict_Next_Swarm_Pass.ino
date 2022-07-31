93dddd90e8b9a5ad8e3db898b6e61d0764f981d9
PaulZC <pc235603@gmail.com>
2022-07-31 20:53:57 +0100

    Update Example3_ESP32_Predict_Next_Swarm_Pass.ino

--- examples/Pass_Prediction/Example3_ESP32_Predict_Next_Swarm_Pass/Example3_ESP32_Predict_Next_Swarm_Pass.ino
+++ examples/Pass_Prediction/Example3_ESP32_Predict_Next_Swarm_Pass/Example3_ESP32_Predict_Next_Swarm_Pass.ino
@@ -1,7 +1,7 @@
 /*
   Predict the pass of the next Swarm satellite
   By: SparkFun Electronics / Paul Clark
-  Date: July 9th, 2022
+  Date: July 31st, 2022
   License: MIT. See license file for more information but you can
   basically do whatever you want with this code.
 
@@ -15,37 +15,65 @@
   
   ** If you have enjoyed this code, please consider making a donation to CelesTrak: https://celestrak.org/ **
 
-  This example is written for the SparkFun Thing Plus C (SPX-18018) but can be adapted
-  for any ESP32 board.
+  This example is written for the SparkFun Thing Plus C but can be adapted for any ESP32 board.
 
-  Use the Boards Manager to install the "ESP32 Arduino" boards.
-  
-  Add https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
-  to your "Preferences \ Additional Boards Manager URLs" if required.
-  
-  Select "ESP32 Dev Module" as the Board (not "SparkFun ESP32 Thing Plus").
-
-  If the SD card is not detected ("Card Mount Failed"), try adding a pull-up resistor between
-  19/CIPO and 3V3. A resistor in the range 10K - 1M seems to help.
+  If the SD card is not detected ("Card Mount Failed"), try adding a 10K pull-up resistor between 19/POCI and 3V3.
 
   Feel like supporting open source hardware?
   Buy a board from SparkFun!
-  SparkFun Thing Plus C - ESP32 WROOM: https://www.sparkfun.com/products/18018
+  SparkFun Thing Plus C - ESP32 WROOM
 
 */
+
+//#define noModemGeospatial // Uncomment this line if you want to define your own location and time for the pass predictions
+
+#ifdef noModemGeospatial
+
+unsigned long unixTime = 1659225600; //                   <-- Update this with the current Unix time if desired
+
+// See https://www.unixtimestamp.com/
+// 1659225600 = Sun Jul 31 2022 00:00:00 GMT+0000
+
+double myLat = 55.000; // Latitude 55 degrees North       <-- Update this with your latitude if desired
+double myLon = -1.000; // Longitude 1 degree West         <-- Update this with your longitude if desired
+double myAlt = 100.00; // Altitude 100m Above Sea Level   <-- Update this with your altitude if desired
+
+#else
+
+#include <SparkFun_Swarm_Satellite_Arduino_Library.h> //Click here to get the library:  http://librarymanager/All#SparkFun_Swarm_Satellite
+
+SWARM_M138 mySwarm;
+
+#if defined(ARDUINO_ESP32_DEV)
+// If you are using the ESP32 Dev Module board definition, you need to create the HardwareSerial manually:
+#pragma message "Using HardwareSerial for M138 communication - on ESP32 Dev Module"
+HardwareSerial swarmSerial(2); //TX on 17, RX on 16
+#else
+// Serial1 is supported by the new SparkFun ESP32 Thing Plus C board definition
+#pragma message "Using Serial1 for M138 communication"
+#define swarmSerial Serial1 // Use Serial1 to communicate with the modem. Change this if required.
+#endif
+
+// If you are using the Swarm Satellite Transceiver MicroMod Function Board:
+//
+// The Function Board has an onboard power switch which controls the power to the modem.
+// The power is disabled by default.
+// To enable the power, you need to pull the correct PWR_EN pin high.
+//
+// Uncomment and adapt a line to match your Main Board and Processor configuration:
+//#define swarmPowerEnablePin A1 // MicroMod Main Board Single (DEV-18575) : with a Processor Board that supports A1 as an output
+//#define swarmPowerEnablePin 39 // MicroMod Main Board Single (DEV-18575) : with e.g. the Teensy Processor Board using pin 39 (SDIO_DATA2) to control the power
+//#define swarmPowerEnablePin 4  // MicroMod Main Board Single (DEV-18575) : with e.g. the Artemis Processor Board using pin 4 (SDIO_DATA2) to control the power
+//#define swarmPowerEnablePin G5 // MicroMod Main Board Double (DEV-18576) : Slot 0 with the ALT_PWR_EN0 set to G5<->PWR_EN0
+//#define swarmPowerEnablePin G6 // MicroMod Main Board Double (DEV-18576) : Slot 1 with the ALT_PWR_EN1 set to G6<->PWR_EN1
+
+#endif
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 #include <Sgp4.h> //Click here to get the library: http://librarymanager/All#SparkFun_SGP4_Arduino_Library
 
 Sgp4 sat;
-
-unsigned long unixTime = 1657324800; //                   <-- Update this with the current Unix time
-
-// See https://www.unixtimestamp.com/
-// 1657324800 = Sat Jul 09 2022 00:00:00 GMT+0000
-
-double myLat = 55.000; // Latitude 55 degrees North       <-- Update this with your latitude
-double myLon = -1.000; // Longitude 1 degree West         <-- Update this with your longitude
-double myAlt = 100.00; // Altitude 100m Above Sea Level   <-- Update this with your altitude
 
 // Set the minimum satellite elevation (above the horizon)
 // Passes lower than this are ignored
@@ -64,6 +92,11 @@ double minimumElevation = 15.0; //                        <-- Update this if req
 
 void setup()
 {
+  // Swarm Satellite Transceiver MicroMod Function Board PWR_EN
+  #ifdef swarmPowerEnablePin
+  pinMode(swarmPowerEnablePin, OUTPUT); // Enable modem power 
+  digitalWrite(swarmPowerEnablePin, HIGH);
+  #endif
 
   delay(1000);
 
@@ -75,9 +108,79 @@ void setup()
   while (!Serial.available()); // Wait for a keypress
   
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  // Wait for the modem to get a GPS fix - if desired
+  
+#ifndef noModemGeospatial
+  //mySwarm.enableDebugging(); // Uncomment this line to enable debug messages on Serial
+
+  bool modemBegun = mySwarm.begin(swarmSerial); // Begin communication with the modem
+  
+  while (!modemBegun) // If the begin failed, keep trying to begin communication with the modem
+  {
+    Serial.println(F("Could not communicate with the modem. It may still be booting..."));
+    delay(2000);
+    modemBegun = mySwarm.begin(swarmSerial);
+  }
+
+  // Call getDateTime to request the most recent Date/Time
+  Swarm_M138_DateTimeData_t dateTime; // Allocate memory for the Date/Time
+  
+  Swarm_M138_Error_e err = mySwarm.getDateTime(&dateTime);
+
+  while (err != SWARM_M138_SUCCESS)
+  {
+    Serial.print(F("Swarm communication error: "));
+    Serial.print((int)err);
+    Serial.print(F(" : "));
+    Serial.println(mySwarm.modemErrorString(err)); // Convert the error into printable text
+    Serial.println(F("The modem may not have acquired a valid GPS date/time reference..."));
+    delay(2000);
+    err = mySwarm.getDateTime(&dateTime);
+  }
+
+  Serial.print(F("getDateTime returned: "));
+  Serial.print(dateTime.YYYY);
+  Serial.print(F("/"));
+  Serial.print(dateTime.MM);
+  Serial.print(F("/"));
+  Serial.print(dateTime.DD);
+  Serial.print(F(" "));
+  Serial.print(dateTime.hh);
+  Serial.print(F(":"));
+  Serial.print(dateTime.mm);
+  Serial.print(F(":"));
+  Serial.println(dateTime.ss);
+
+  // Call getGeospatialInfo to request the most recent geospatial information
+  Swarm_M138_GeospatialData_t info;
+  
+  err = mySwarm.getGeospatialInfo(&info);
+  
+  while (err != SWARM_M138_SUCCESS)
+  {
+    Serial.print(F("Swarm communication error: "));
+    Serial.print((int)err);
+    Serial.print(F(" : "));
+    Serial.println(mySwarm.modemErrorString(err)); // Convert the error into printable text
+    Serial.println(F("The modem may not have acquired a valid GPS fix..."));
+    delay(2000);
+    err = mySwarm.getGeospatialInfo(&info);
+  }
+
+  Serial.print(F("getGeospatialInfo returned: "));
+  Serial.print(info.lat, 4);
+  Serial.print(F(","));
+  Serial.print(info.lon, 4);
+  Serial.print(F(","));
+  Serial.println(info.alt);
+
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   // Set site latitude[°], longitude[°] and altitude[m]
 
+  sat.site(info.lat, info.lon, info.alt);
+#else
   sat.site(myLat, myLon, myAlt);
+#endif
   
   //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   // Begin the SD card and open the file
@@ -108,6 +211,8 @@ void setup()
 
     // Allocate 30 bytes to store the satellite name
     char satelliteName[30];
+    for (int i = 0; i < 30; i++)
+      satelliteName[i] = 0;
 
     // Read the satellite name
     int satNameLength = tleFile.readBytesUntil('\n', (char *)satelliteName, 30);
@@ -144,10 +249,15 @@ void setup()
 
       sat.init(satelliteName, lineOne, lineTwo); //initialize satellite parameters     
 
+#ifndef noModemGeospatial
+      mySwarm.getDateTime(&dateTime);
+      unsigned long unixTime = getUnixEpoch(dateTime);
+#endif
+
       double passStartJD;
       if (Predict(unixTime, &passStartJD)) //Calculates the next overpass
       {
-        if (passStartJD < nextPassJD)
+        if ((passStartJD < nextPassJD) && (passStartJD > getJulianFromUnix(unixTime))) // Record the next pass (if not already started)
         {
           nextPassJD = passStartJD;
         }
@@ -185,8 +295,8 @@ void loop()
 // Return true if the prediction was successful.
 bool Predict(unsigned long startTime, double *overpassStart)
 {
-  passinfo overpass;                       //structure to store overpass info
-  sat.initpredpoint( startTime , 0.0 );    //finds the startpoint. startTime is Unix time (not Julian Day)
+  passinfo overpass; //structure to store overpass info
+  sat.initpredpoint( startTime , 0.0 ); //finds the startpoint. startTime is Unix time (not Julian Day)
   
   bool error;
   int year; int mon; int day; int hr; int minute; double sec;
@@ -214,3 +324,44 @@ bool Predict(unsigned long startTime, double *overpassStart)
 
   return (error == 1);
 }
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef noModemGeospatial
+
+// Convert GPS (UTC) time to Unix epoch
+
+const uint32_t SFE_UBLOX_DAYS_FROM_1970_TO_2020 = 18262; // Jan 1st 2020 Epoch = 1577836800 seconds
+const uint16_t SFE_UBLOX_DAYS_SINCE_2020[80] =
+{
+  0, 366, 731, 1096, 1461, 1827, 2192, 2557, 2922, 3288,
+  3653, 4018, 4383, 4749, 5114, 5479, 5844, 6210, 6575, 6940,
+  7305, 7671, 8036, 8401, 8766, 9132, 9497, 9862, 10227, 10593,
+  10958, 11323, 11688, 12054, 12419, 12784, 13149, 13515, 13880, 14245,
+  14610, 14976, 15341, 15706, 16071, 16437, 16802, 17167, 17532, 17898,
+  18263, 18628, 18993, 19359, 19724, 20089, 20454, 20820, 21185, 21550,
+  21915, 22281, 22646, 23011, 23376, 23742, 24107, 24472, 24837, 25203,
+  25568, 25933, 26298, 26664, 27029, 27394, 27759, 28125, 28490, 28855
+};
+const uint16_t SFE_UBLOX_DAYS_SINCE_MONTH[2][12] =
+{
+  {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335}, // Leap Year (Year % 4 == 0)
+  {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334}  // Normal Year
+};
+
+uint32_t getUnixEpoch(Swarm_M138_DateTimeData_t dateTime)
+{
+  uint32_t t = SFE_UBLOX_DAYS_FROM_1970_TO_2020;                                              // Jan 1st 2020 as days from Jan 1st 1970
+  t += (uint32_t)SFE_UBLOX_DAYS_SINCE_2020[dateTime.YYYY - 2020];                             // Add on the number of days since 2020
+  t += (uint32_t)SFE_UBLOX_DAYS_SINCE_MONTH[dateTime.YYYY % 4 == 0 ? 0 : 1][dateTime.MM - 1]; // Add on the number of days since Jan 1st
+  t += (uint32_t)dateTime.DD - 1;                                                             // Add on the number of days since the 1st of the month
+  t *= 24;                                                                                    // Convert to hours
+  t += (uint32_t)dateTime.hh;                                                                 // Add on the hour
+  t *= 60;                                                                                    // Convert to minutes
+  t += (uint32_t)dateTime.mm;                                                                 // Add on the minute
+  t *= 60;                                                                                    // Convert to seconds
+  t += (uint32_t)dateTime.ss;                                                                 // Add on the second
+  return (t);
+}
+
+#endif
